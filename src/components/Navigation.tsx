c41e0f336986486e5bef8498a23e99006702c69d
gunayakay <gunayakay@outlook.com>
2024-03-21 21:43:07 +0300

    test

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -6,36 +6,43 @@ import web from '@assets/img/web.png';
 
 export default function Navigation() {
   const [show, setShow] = useState<boolean>(false);
-  // const toggleNavigation = useCallback(() => setShow(prev => !prev), []);
+  const toggleNavigation = useCallback(() => setShow(prev => !prev), []);
   return (
-    <nav className="fixed  top-0 left-0 w-full h-[100vh]  inline-block transition-all visible opacity-100 z-[9999999] bg-black/90">
-      <div className="fixed pt-[30px] pb-[50px] pl-4 h-full bg-[#1e1e1e]  z-[100]   overflow-y-scroll w-[720px] max-w-full">
-        <ul className="relative min-h-[12px] pr-[73px]">
-          <li className="relative -top-[2px] float-left ">
-            <a href="#">
+    <nav class="fixed  top-0 left-0 w-full h-[100vh] inline-block transition-all  visible opacity-100 z-[9999999] bg-black/90">
+      <div class="fixed pt-[30px] pb-[50px] pl-4 h-full bg-[#1e1e1e]  z-[100] opacity-100 visible  overflow-y-scroll w-[720px] max-w-full">
+        <ul class="relative min-h-[12px] pr-[73px]">
+          <li class="relative -top-[2px] float-left ">
+            <a class="cursor-pointer text-white" href="#">
               <img src={close.src} alt="Close Icon" />
             </a>
           </li>
-          <li className="absolute mt-[9px] left-[49%]">
+          <li class="absolute mt-[9px]  left-[49%]">
             <a href="#">
               <img src={navLogo.src} alt="Close Icon" />
             </a>
           </li>
-          {/* <li className="relative float-right min-w-[80px] border-[1px] border-[#d7d2bd] rounded leading-[26px] h-[26px] mt-2 p-2 after:content-[''] after:border-solid after:border-[5px]  ">
+          {/* <li class="relative float-right min-w-[80px] border-[1px] border-[#d7d2bd] rounded leading-[26px] h-[26px] mt-2 p-2 after:content-[''] after:border-solid after:border-[5px]  ">
             <Image src={web} alt="Web Icon" />
             <select
-              className="border-none text-[#d7d2bd] px-[10px] max-w-[50px] leading-[1px] h-[14px] text-[13px]"
+              class="border-none text-[#d7d2bd] px-[10px] max-w-[50px] leading-[1px] h-[14px] text-[13px]"
               name=""
               id="languages"
             >
-              <option className="bg-black" value="tr">
+              <option class="bg-black" value="tr">
                 TR
               </option>
-              <option className="bg-black" value="en">
+              <option class="bg-black" value="en">
                 EN
               </option>
             </select>
           </li> */}
+        </ul>
+        <ul class="mt-12">
+          <li class="relative leading-10 mb-4">
+            <span class="relative text-[#969696] text-3xl block cursor-pointer pl-5 before:content-[''] before:absolute before:h-[30px] before:w-[1px] before:opacity-0 before:top-[6px] before:-left-[1px] before:bg-white">
+              Anasayfa
+            </span>
+          </li>
         </ul>
       </div>
     </nav>
