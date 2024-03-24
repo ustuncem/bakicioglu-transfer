59c1fdb6ad7cc0fd11078810fc4c4f0c37a8755d
gunayakay <gunayakay@outlook.com>
2024-03-24 21:17:23 +0300

    add the index.ts to components

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export {default as Container} from './Container.astro';
+export {default as Header} from './Header.astro';
+export {default as Info} from './Info.astro';
+export {default as NavigationLink} from './NavigationLink';
+export {default as Navigation} from './Navigation';
+export {default as NavigationToggle} from './NavigationToggle';
